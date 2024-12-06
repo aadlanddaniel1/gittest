6cbc11b1b8003bd4c622e37c328599f0cfd670a0
danielaadland1 <543-student001@la-study.com>
2024-12-06 16:12:13 +0900

    メッセージ追加

--- src/gittest/Hello.java
+++ src/gittest/Hello.java
@@ -2,6 +2,7 @@ package gittest;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("hello world");
+		System.out.println("hello");
+		System.out.println("world");
 	}
 }
